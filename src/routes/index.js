6473a2489dcd7822e251ce5dfed24d4dc631c2c2
minjikim <alswl1500@naver.com>
2021-12-15 13:39:43 +0900

    feat: 없는 페이지에 대한 설정

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,16 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import LoginPage from '../views/LoginPage.vue';
 import SignupPage from '../views/SignupPage.vue';
-
+import NotFoundPage from '../views/NotFoundPage.vue';
 Vue.use(VueRouter)
 
 export const router = new VueRouter({
   mode: 'history',
   routes: [
+    {
+      path: '/',
+      redirect: '/login',
+    },
     {
       path: '/login',
       name: 'loginPage',
@@ -17,6 +21,10 @@ export const router = new VueRouter({
       path: '/signup',
       name: 'signupPage',
       component: SignupPage,
+    },
+    {
+      path: '*',
+      component: NotFoundPage,
     }
   ]
 })
